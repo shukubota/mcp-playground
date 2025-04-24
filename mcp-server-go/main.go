35991311d16b15d19c52687ac0ad2bcafd030a17
shukubota <kubotashu0421@gmail.com>
2025-04-24 19:56:15 +0900

    feat: implement echo tool in MCP server with error handling

--- mcp-server-go/main.go
+++ mcp-server-go/main.go
@@ -1,33 +1,61 @@
 package main
 
 import (
-	"log"
+	"context"
+	"errors"
+	"fmt"
 	"os"
+	"strings"
 
+	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
 )
 
 func main() {
 	err := run()
 	if err != nil {
-		log.Printf("Error running the server: %+v", err)
 		os.Exit(1)
 	}
+	// if err != nil {
+	// 	log.Printf("Error running the server: %+v", err)
+	// 	os.Exit(1)
+	// }
 }
 
 func run() error {
 	s := server.NewMCPServer(
 		"demo",
 		"1.0.0",
-		server.WithResourceCapabilities(true, true),
+		// server.WithResourceCapabilities(true, true),
 		server.WithLogging(),
 		server.WithRecovery(),
 	)
 
+	// fmt.Println(s)
+
 	// tools := mcp.NewTool("calculate")
 
-	if err := server.ServeStdio(s); err != nil {
-		return err
+	tool := mcp.NewTool(
+		"echo",
+		mcp.WithDescription("echo tool"),
+		mcp.WithNumber("count", mcp.Required(), mcp.Description("count of echo")),
+		mcp.WithString("message", mcp.Required(), mcp.Description("message to echo")),
+	)
+
+	f := func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+		message := request.Params.Arguments["message"].(string)
+		count := request.Params.Arguments["count"].(float64)
+
+		return mcp.NewToolResultText(strings.Repeat(message, int(count))), nil
+	}
+
+	s.AddTool(tool, f)
+
+	// fmt.Println("Starting server...")
+
+	err := server.ServeStdio(s)
+	if err != nil && !errors.Is(err, context.Canceled) {
+		return fmt.Errorf("failed to serve: %w", err)
 	}
 	return nil
 }
